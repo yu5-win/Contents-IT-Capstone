4c74dddbaed6510b3702f1730fe2cff9a65eddb0
rhekdiahs <rhekdiahs@gmail.com>
2021-06-08 16:43:09 +0900

    balances

---
+++ balances/sumweight.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sumweight : MonoBehaviour
+{
+    public measureWeight[] ob;
+    public float[] mass = new float[5];
+    public GameObject text;
+
+    public void check()
+    {
+        if(ob[0].is_OnPosition && ob[1].is_OnPosition && ob[2].is_OnPosition && ob[3].is_OnPosition && ob[4].is_OnPosition)
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                mass[i] = ob[i].GetObjectMass();
+            }
+            if(mass[0] > mass[1] && mass[1] > mass[2] && mass[2] > mass[3] && mass[3] > mass[4])
+            {
+                Debug.Log("success");
+                text.SetActive(true);
+            }
+            else
+            {
+                Debug.Log("fail");
+            }
+        }
+        else
+        {
+            Debug.Log("fail");
+        }
+    }
+}
